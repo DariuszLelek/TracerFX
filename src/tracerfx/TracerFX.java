08e296bb08c960083dc5ed945ac4ec06eb29b772
Dariusz Lelek <dariusz.lelek@gmail.com>
2017-08-04 18:12:31 +0100

    Project created

---
+++ src/tracerfx/TracerFX.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2017 Dariusz Lelek.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tracerfx;
+
+import java.io.IOException;
+import java.net.URL;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import tracerfx.fxml.FXMLResourceLoader;
+
+/**
+ *
+ * @author Dariusz Lelek
+ */
+public class TracerFX extends Application {
+    private static Parent root;
+    private static URL resource;
+    
+    private final static String TITLE = "TracerFX";
+    private final static String FXML = "tracerfx/fxml/TracerFXML.fxml";
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        prepare();
+        
+        primaryStage.setTitle(TITLE);
+        primaryStage.setScene(getScene());
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    private void prepare() throws IOException{
+        ClassLoader classLoader = getClass().getClassLoader();
+        resource = classLoader.getResource(FXML);
+        root = FXMLResourceLoader.getResource(resource);
+    }
+    
+    private Scene getScene() throws NullPointerException{
+        if(root == null){
+            throw new NullPointerException();
+        }
+        return new Scene(root);
+    } 
+}
